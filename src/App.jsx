2c76a998ff11fd17ed440e4da645d7ad626b484c
Rushil Patel <rushilpatel@Rushils-MacBook-Air.local>
2025-02-15 15:24:41 -0500

    project init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <h1 class="text-3xl font-bold underline">
+        Scribe-it
+      </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
