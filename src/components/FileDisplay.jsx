e35ac16c1b546a359efa859806b9d628a3d92c8e
Rushil Patel <rushilpatel@Rushils-MacBook-Air.local>
2025-02-16 13:22:38 -0500

    fixed errors with start/stop recording

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -4,15 +4,15 @@ export default function FileDisplay(props) {
     const { handleAudioReset, file, audioStream } = props;
     
     return (
-        <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 justify-center pb-20 w-fit max-w-full mx-auto'>
+        <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 justify-center pb-20 w-72 max-w-full mx-auto'>
             <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
                 <span className="text-blue-400 bold">Your</span>
                 &nbsp;
                 file
             </h1>
-            <div className="mx-auto flex flex-col text-left my-4">
+            <div className="flex flex-col text-left my-4">
                 <h3 className="font-semibold">Name</h3>
-                <p>{file.name}</p>
+                <p>{file? file.name: "Custom Audio"}</p>
             </div>
             <div className="flex items-center justify-between gap-4">
                 <button onClick={handleAudioReset} className="text-slate-400 cursor-pointer hover:text-blue-600 duration-200 flex items-center gap-2">
@@ -20,7 +20,7 @@ export default function FileDisplay(props) {
                     <p>Reset</p>
                 </button>
                 <button className="specialBtn px-4 py-2 rounded-lg text-blue-400 cursor-pointer flex items-center gap-2">
-                    <i class="fa-solid fa-pen-nib"></i>
+                    <i className="fa-solid fa-pen-nib"></i>
                     <p>Transcribe</p>
                 </button>
             </div>
